d5499f599caac8b44b6c8582832c69680a8c0e8a
Monstradamys <dmitrieva.av1337@gmail.com>
2021-02-18 12:55:34 +0300

    in progress

--- src/features/api/subject/get-themes-tree-list.ts
+++ src/features/api/subject/get-themes-tree-list.ts
@@ -4,7 +4,7 @@ import { GetListQueryParams } from '../types'
 export const getThemesTreeListFx = createApiEffect<GetListQueryParams, any>({
   requestMapper: (query) => ({
     method: 'GET',
-    url: '/api/subject/themes/filter-tree/',
+    url: '/api/subject/themes/tree/',
     query,
   }),
 })
