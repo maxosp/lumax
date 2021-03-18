d053aedfc3ac33171143e67ef66ecb39b5bb40fb
Vlad Zhulinskiy <33326216+heltoe@users.noreply.github.com>
2021-03-18 11:07:25 +0300

    Comment modal application (#50)
    * add comments modal to applications page
    
    * fix linter error
    
    Co-authored-by: Monstradamys <dmitrieva.av1337@gmail.com>

---
+++ src/pages/common/modals/applications/CommentsModal.vue
@@ -0,0 +1,127 @@
+<template>
+  <Modal
+    class="dialog"
+    :value="true"
+    @change="$emit('close')"
+  >
+    <Icon
+      type="close"
+      class="modal-icon"
+      size="12"
+      @click="$emit('close')"
+    />
+    <p class="title-modal">Оставить комментарий</p>
+    <div class="text-area-wrapper">
+      <p class="title-textarea">Комментарий</p>
+      <BaseTextarea
+        placeholder="Введите ваш комментарий"
+        :value="text"
+      />
+    </div>
+    <div class="uploader-block">
+      <!-- TODO uploader element -->
+      <div class="wrapper-images">
+        <!-- TODO v-for items -->
+        <FileElement />
+      </div>
+    </div>
+    <div class="btns-wrapper">
+      <BaseButton
+        class="btn"
+        big
+        border-without-bg
+        @change="$emit('close')"
+      >
+        Отменить
+      </BaseButton>
+      <BaseButton
+        class="btn"
+        big
+        @change="$emit('agree')"
+      >
+        Отправить на доработку
+      </BaseButton>
+    </div>
+  </Modal>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+import Modal from '@/ui/modal/Modal.vue'
+import Icon from '@/ui/icon/Icon.vue'
+import BaseButton from '@/ui/button/BaseButton.vue'
+import BaseTextarea from '@/ui/input/BaseTextarea.vue'
+import FileElement from '@/pages/common/modals/applications/parts/FileElement.vue'
+
+export default Vue.extend({
+  name: 'ModalLogout',
+  components: {
+    Modal,
+    Icon,
+    BaseButton,
+    BaseTextarea,
+    FileElement,
+  },
+  data() {
+    return {
+      text: '',
+    }
+  },
+})
+</script>
+
+<style scoped>
+.dialog ::v-deep .modal-body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 30px 20px;
+  position: relative;
+}
+.modal-icon {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+  fill: var(--c-grey-3);
+}
+.title-modal {
+  text-align: center;
+  font-size: 18px;
+  line-height: 22px;
+}
+.text-area-wrapper {
+  margin-top: 30px;
+  width: 100%;
+}
+.title-textarea {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  margin-bottom: 5px;
+}
+.uploader-block {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 10px;
+}
+.btns-wrapper {
+  width: 100%;
+  @mixin flex-row-central;
+  justify-content: center;
+  margin-top: 20px;
+  .btn {
+    &:nth-child(2) {
+      margin-left: 10px;
+    }
+  }
+}
+.wrapper-images {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+</style>
---
+++ src/pages/common/modals/applications/parts/FileElement.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="file-element">
+    <div class="preview" :style="{ backgroundImage: `url(${image})` }" />
+    <p class="name-file">name file</p>
+    <Icon
+      type="close"
+      class="close-icon"
+      size="12"
+      @click="$emit('delete')"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import Icon from '@/ui/icon/Icon.vue'
+
+export default Vue.extend({
+  name: 'ModalLogout',
+  components: { Icon },
+  props: {
+    name: { type: String as PropType<string>, default: '' },
+    image: { type: String as PropType<string>, default: '' },
+  },
+})
+</script>
+
+<style scoped>
+.file-element {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+.preview {
+  width: 30px;
+  height: 30px;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-right: 15px;
+  position: relative;
+  border-radius: 5px;
+  overflow: hidden;
+  &:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+}
+.name-file {
+  font-weight: bold;
+  font-size: 14px;
+}
+.close-icon {
+  cursor: pointer;
+  margin-left: auto;
+  fill: var(--c-grey-3);
+}
+</style>
