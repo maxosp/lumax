3876dea90fdb9a0a02ce88187233ef63d1a64d0c
Roman Kuchmin <37706706+white5pace@users.noreply.github.com>
2021-05-13 15:37:30 +0300

    :bug: fixed comment images names (#214)

--- src/pages/common/modals/applications/parts/FileElement.vue
+++ src/pages/common/modals/applications/parts/FileElement.vue
@@ -7,7 +7,7 @@
         alt="comment image"
       >
     </div>
-    <p class="name-file">name file</p>
+    <p class="name-file">Изображение {{ fileIndex }}</p>
     <Icon
       type="close"
       class="close-icon"
@@ -25,9 +25,14 @@ export default Vue.extend({
   name: 'ModalLogout',
   components: { Icon },
   props: {
-    name: { type: String as PropType<string>, default: '' },
+    index: { type: Number as PropType<number> },
     image: { type: String as PropType<string>, default: '' },
     id: { type: Number as PropType<number> },
+  },
+  computed: {
+    fileIndex() {
+      return this.index !== undefined ? this.index + 1 : ''
+    },
   },
 })
 </script>
