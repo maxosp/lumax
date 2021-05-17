83ff3b9339ac4249951940b87424333244c9126e
Monstradamys <dmitrieva.av1337@gmail.com>
2021-05-17 16:23:20 +0300

    fix

--- src/features/api/media/download-media-file.ts
+++ src/features/api/media/download-media-file.ts
@@ -1,6 +1,6 @@
 import { createApiEffect } from '@/features/api/common/create-api-effect'
 
-export const downloadMediaFileFx = createApiEffect<number>({
+export const downloadMediaFileFx = createApiEffect<number, Response>({
   requestMapper: (id) => ({
     method: 'GET',
     url: `/api/media-app/media/${id}/file/`,
