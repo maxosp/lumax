bdac5b79af2b9916a09a68136835f4c0bed7ef57
Maxim Osipenko <37467552+maxosp@users.noreply.github.com>
2021-05-31 14:39:22 +0300

    fix: fixed loading labels list by theme (#298)
    * feat: embeded video support
    
    * fix: fixed placeholder when none tasks for label
    
    * fix: fixed loading labels list by theme
    
    Co-authored-by: shurfl <sshurfll@gmail.com>
    Co-authored-by: Maxim Osipenko <maxospin@gmail.com>

--- src/pages/bank/test-tasks/create/parts/labels-dropdown/labels-dropdown.model.ts
+++ src/pages/bank/test-tasks/create/parts/labels-dropdown/labels-dropdown.model.ts
@@ -1,24 +1,35 @@
-import { createEvent, createStore, forward, attach, restore, combine } from 'effector-root'
+import { createEvent, createStore, forward, attach, restore, combine, merge } from 'effector-root'
+import { debounce, every } from 'patronum'
 import { createFilter } from '@/pages/common/filter-dropdown/create-filter'
 import { DropdownItem } from '@/pages/common/types'
-import { $selectedSubject } from '@/pages/common/dropdowns/subject/subjects-dropdown.model'
-import { $selectedClass } from '@/pages/common/dropdowns/class/classes-dropdown.model'
-import { debounce, every } from 'patronum'
-import { $selectedTheme } from '@/pages/common/dropdowns/themes-tree/theme-dropdown.model'
-import { getLabelsTreeFx } from '@/features/api/assignment/labels/get-labels-tree'
+import {
+  $selectedSubject,
+  setSelectedSubject,
+} from '@/pages/common/dropdowns/subject/subjects-dropdown.model'
+import {
+  $selectedClass,
+  setSelectedClass,
+} from '@/pages/common/dropdowns/class/classes-dropdown.model'
+import {
+  $selectedTheme,
+  setSelectedTheme,
+} from '@/pages/common/dropdowns/themes-tree/theme-dropdown.model'
+import { getLabelsListFx } from '@/features/api/assignment/labels/get-labels-list'
 
 export const labelsDropdownModule = createFilter()
 
 const getLabels = attach({
-  effect: getLabelsTreeFx,
+  effect: getLabelsListFx,
 })
 
 export const loadLabels = createEvent<void>()
 export const $labels = createStore<DropdownItem[]>([])
 
+const resetLabels = merge([setSelectedClass, setSelectedSubject, setSelectedTheme])
+
 export const setSelectedLabels = createEvent<DropdownItem[]>()
 export const clearSelectedLabels = createEvent<void>()
-export const $selectedLabels = restore(setSelectedLabels, [])
+export const $selectedLabels = restore(setSelectedLabels, []).reset(resetLabels)
 
 export const $canSetLabels = every({
   predicate: (value) => value !== null,
@@ -61,6 +72,13 @@ forward({
 })
 
 forward({
-  from: getLabels.doneData.map((res) => res.body.data as unknown as DropdownItem[]),
+  from: getLabels.doneData.map((res) =>
+    (res.body.data as unknown as DropdownItem[]).map((label) => {
+      return {
+        ...label,
+        title: label.name,
+      }
+    })
+  ),
   to: [$labels, labelsDropdownModule.methods.setItems],
 })
